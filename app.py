53a5f6aad11bb21214690003ceccb7869ea51fba
Tejeswar Koti <tejeswarkoti28@gmail.com>
2026-06-01 18:55:34 +0530

    updates

--- app.py
+++ app.py
@@ -146,3 +146,5 @@ if __name__ == "__main__":
     host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run("app:app", host=host, port=port, reload=False, log_level="info")
+
+#-------------------------------------------------------------------------------
